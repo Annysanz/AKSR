b13e339991489e690b96737381240b21e2417de4
Annysanz <annysanz17@gmail.com>
2022-01-28 22:11:26 +0100

    en es show

--- docs/js/en-es.js
+++ docs/js/en-es.js
@@ -1,17 +1,12 @@
-var hide_button = document.getElementById('en');
-var show_button = document.getElementById('es');
+var hide_button = document.getElementById('ee');
+var show_button = document.getElementById('en');
 
-// escuchamos el evento click del botón ocultar
 hide_button.addEventListener('click', function() {
-    // mostramos el botón de mostrar
     show_button.style.display = 'block';
-    // ocultamos el botón de ocultar
     hide_button.style.display = 'none';
 });
 
 show_button.addEventListener('click', function() {
-    // mostramos el botón de mostrar
     show_button.style.display = 'none';
-    // mostramos el botón de mostrar
     hide_button.style.display = 'block';
 });
