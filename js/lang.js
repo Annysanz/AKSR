9834924d42b447e3359da36f7d70e17b3a7eb803
Annysanz <annysanz17@gmail.com>
2022-03-02 20:43:08 +0100

    lang json

--- js/lang.js
+++ js/lang.js
@@ -1,12 +1,12 @@
 $.getJSON('./language/lang.json', function(json){
 
-    if(!localStorage.getItem("lang")){
-        localStorage.setItem("lang", "en");
+    if(!localStorage.getItem("lang", "es")){
+        // localStorage.setItem("lang", "en");
     }
-    let def = localStorage.getItem("lang");
-    $('.lang').each(function(index,value){
-        $(this).text(json[def][$(this).attr('key')]);
-    })
+    // let def = localStorage.getItem("lang");
+    // $('.lang').each(function(index,value){
+    //     $(this).text(json[def][$(this).attr('key')]);
+    // })
 
     $('.translate').click(function(){
         let lang = $(this).attr('id');
