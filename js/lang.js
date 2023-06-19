d2216d8529ed02cd1509f00082c2b83fcabb0b33
Annysanz <annysanz17@gmail.com>
2023-06-19 13:31:55 +0200

    actualizacion lang

--- js/lang.js
+++ js/lang.js
@@ -1,19 +1,19 @@
-$.getJSON('./language/lang.json', function(json){
+$.getJSON('./language/lang.json', (json) => {
 
-    if(!localStorage.getItem("lang", "es")){
-        // localStorage.setItem("lang", "en");
-    }
-    // let def = localStorage.getItem("lang");
-    // $('.lang').each(function(index,value){
-    //     $(this).text(json[def][$(this).attr('key')]);
-    // })
+        if (!localStorage.getItem("lang")) {
+            localStorage.setItem("lang", "en");
+        }
+        let def = localStorage.getItem("lang");
+        $('.lang').each(function (index, value) {
+            $(this).text(json[def][$(this).attr('key')]);
+        });
 
-    $('.translate').click(function(){
-        let lang = $(this).attr('id');
-        localStorage.setItem("lang", lang);
-        $('.lang').each(function(index,value){
-            $(this).text(json[lang][$(this).attr('key')]);
-        })
+        $('.translate').click(function () {
+            let lang = $(this).attr('id');
+            localStorage.setItem("lang", lang);
+            $('.lang').each(function (index, value) {
+                $(this).text(json[lang][$(this).attr('key')]);
+            });
+        });
     })
-})
 
